a957dda358fe261320ab87317a2a185d36d3a4d5
mansiSharma12 <99728671+mansiSharma12@users.noreply.github.com>
2022-03-07 22:56:17 +0530

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "mansi";
+$dbname = "e_classrooms";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Classroom</title>
+    
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="stylesheets/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
+
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <!-- Header hello mansi -->
+    <div class="ui top fixed menu " >
+        <div class="item " >
+            <img style="width:4.5em" class="logo-img " src="images/logo.png">
+        </div>
+        <a class="item "href="index.php">
+            <div style="font-size:25px; "class="ui  black header">BV VIRTUAL CLASSROOM</div>
+        </a>
+        <div class="right menu">
+            <a class="item" href="views/login.php">
+                <div style="font-size:30px;"class="ui header">Login</div>
+            </a>        
+            <a class="item" href="views/register.php">
+                <div style="font-size:30px;"class="ui header">Register</div>
+            </a>
+        </div>
+    </div>
+   <br><br>
+    <div class="ui placeholder teal segment">
+        <div class="heading-container">
+            <div class="ui grid">
+                <div class="nine wide column">
+                    <div class="heading">
+                        <div class="ui teal header">Welcome to BV CLASSROOM!</div>
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="ui medium grey header">
+                        Making learning easy and reachable to all.
+                    </div>
+                </div>
+                <div class="seven wide column">
+                    <img class="ui medium centered huge image" src="images/pagelogo1.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Features Section -->
+    <div class="ui center aligned container">
+        <div class="section-header">
+            <div class="ui huge header">
+                Classroom Features
+            </div>
+            <div class="ui divider"></div>
+        </div>
+        <div class="ui three raised cards">
+            <a class="teal card">
+                <div class="content">
+                    <div class="ui teal large header">
+                        Class Creation
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="description">
+                        <p>Quickly create and Join classes with an auto-generated unique code, wheather for school, universities or your private tuition.</p>
+                    </div>
+                </div>
+            </a>
+            <a class="teal card">
+                <div class="content">
+                    <div class="ui teal large header">
+                        Easy Assignment Submission
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="description">
+                        <p>Effortless interface for fast assignment creation by teacher and submission by the students.</p>
+                    </div>
+                </div>
+            </a><a class="teal card">
+                <div class="content">
+                    <div class="ui teal large header">
+                        Inbuilt Grading System
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="description">
+                        <p>An easy grading system available alongside the submitted assignments with an integrated percentage calculating system.</p>
+                    </div>
+                </div>
+            </a>
+            <a class="teal card">
+                <div class="content">
+                    <div class="ui teal large header">
+                        Weekly Time-Tables
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="description">
+                        <p>Create and view your weekly schedule by adding day-wise tasks.</p>
+                    </div>
+                </div>
+            </a>
+            <a class="teal card">
+                <div class="content">
+                    <div class="ui teal large header">
+                        Study Groups
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="description">
+                        <p>Special feature for students to discuss a class and share notes. Easily create and join study groups with your classmates and friends.</p>
+                    </div>
+                </div>
+            </a><a class="teal card">
+                <div class="content">
+                    <div class="ui teal large header">
+                        Staff Rooms
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="description">
+                        <p>Special feature for teachers to create and join groups and discuss and share information with other teachers. </p>
+                    </div>
+                </div>
+            </a>
+            <a class="teal card">
+                <div class="content">
+                    <div class="ui teal large header">
+                        Vacccination Status
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="description">
+                        <p>Know the covid vaccination status of all your friends and students in a given class.</p>
+                    </div>
+                </div>
+            </a>
+            <a class="teal card">
+                <div class="content">
+                    <div class="ui teal large header">
+                        Clean and neat UI
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="description">
+                        <p>An easy to navigate and clean user-friendly UI for anyone to use.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <br><br>
+    <!-- FAQ's Section 
+    <div class="ui center aligned container">
+        <div class="section-header">
+            <div class=" ui huge header">
+                Frequently Asked Questions
+            </div>
+        </div>
+    </div>
+    <div class="ui container">
+        <div class="ui teal segment">
+            <div class="ui header">
+                What is Octocat Classes?
+            </div>
+        </div>
+        <div class="ui teal segment">
+            <div class="ui header">
+                Is Octocat Classes free?
+            </div>
+        </div>
+        <div class="ui teal segment">
+            <div class="ui header">
+                Is Octocat Classes for schools and universities or can be used for private tuition?
+            </div>
+        </div>
+        <div class="ui teal segment">
+            <div class="ui header">
+                Can I use Octocat Classes on my phone?
+            </div>
+        </div>
+
+    </div>-->
+    <!-- Meeet our creator Section -->
+    <div class="ui center aligned container">
+        <div class="section-header">
+            <div class=" ui black huge header">
+                Meet Our Creators
+            </div>
+            <div class="ui divider"></div>
+        </div>
+    </div>
+    <div class="ui container">
+        <div class="ui grid">
+            <div class=" row">
+                <div class="one wide column"></div>
+                <div class="two wide column">
+                    <img class="ui tiny circular centered image" src="images/partner1.jpg">
+                </div>
+                <div class="five wide column">
+                    <div class="ui teal header">
+                        MANSI SHARMA
+                        <div class="sub header">
+                            Third year Computer Science student at Banasthali Vidyapith,Rajasthan.
+                        </div>
+                    </div>
+                    <div class="ui small star rating"></div>
+                </div>
+                <div class="two wide column">
+                    <img class="ui tiny circular centered image" src="images/partner2.jpg">
+                </div>
+                <div class="five wide column">
+                    <div class="ui teal header">
+                        MANAVI KUMARI
+                        <div class="sub header">
+                            Third year Information Technology student at Banasthali Vidyapith,Rajasthan.
+                        </div>
+                    </div>
+                    <div class="ui small star rating"></div>
+                </div>
+            </div>
+            <div class=" row">
+            <div class="one wide column"></div>
+                <div class="two wide column">
+                    <img class="ui tiny circular centered image" src="images/partner3.jpg">
+                </div>
+                <div class="five wide column">
+                    <div class="ui teal header">
+                        MANSI YADAV
+                        <div class="sub header">
+                            Third year Information Technology student at Banasthali Vidyapith,Rajasthan.
+                        </div>
+                    </div>
+                    <div class="ui small star rating"></div>
+                </div>
+                <div class="two wide column">
+                    <img class="ui tiny circular centered image" src="images/partner4.jpg">
+                </div>
+                <div class="five wide column">
+                    <div class="ui teal header">
+                        KIRAN KANWAR
+                        <div class="sub header">
+                            Third year Information Technology student at Banasthali Vidyapith,Rajasthan.
+                        </div>
+                    </div>
+                    <div class="ui small star rating"></div>
+                </div>
+            </div>
+            <!-- <div class="ui divider"></div>
+            <div class="row">
+                <div class="four wide column">
+                    <img class="ui tiny circular centered image" src="images/student.png">
+                </div>
+                <div class="twelve wide column">
+                    <div class="ui header">
+                        Sahil chandel
+                        <div class="sub header">
+                            Final year Computer Science student at National Institute of Technology, Hamirpur.
+                        </div>
+                    </div>
+                    <div class="ui small star rating"></div>
+                </div>
+            </div> -->
+        </div>
+    </div>
+    <br><br>
+    <!-- Footer -->
+    <div class="ui fluid center aligned container placeholder segment">
+        <div class="ui header">
+            <div class="sub header">
+                <h1>
+                    <a><i class="facebook icon"></i></a>
+                    <a><i class="instagram icon"></i></a>
+                    <a><i class="twitter square icon"></i></a>
+                </h1>
+            </div>
+            Info . Support . Marketting
+            <div class="sub header">
+                Terms of Use . Privacy Policy<br><br>
+                <i class="copyright icon"></i>
+                2022 BV Classes
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js" integrity="sha512-dqw6X88iGgZlTsONxZK9ePmJEFrmHwpuMrsUChjAw1mRUhUITE5QU9pkcSox+ynfLhL15Sv2al5A0LVyDCmtUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+</body>
+
+</html>
